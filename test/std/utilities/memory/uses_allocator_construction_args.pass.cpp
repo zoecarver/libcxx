4fd38113b8541e6739ff31b0ea2835e5d86c3671
zoe <z.zoelec2@gmail.com>
2019-04-24 11:40:22 -0700

    stash: formatting

--- test/std/utilities/memory/uses_allocator_construction_args.pass.cpp
+++ test/std/utilities/memory/uses_allocator_construction_args.pass.cpp
@@ -17,46 +17,46 @@ using pair_t = std::pair<int, double>;
 
 struct A
 {
-	int a;
-	float b;
+    int a;
+    float b;
 
-	A(int a, float b): a(a), b(b) { }
+    A(int a, float b): a(a), b(b) { }
 };
 
 template <class... Args>
 void test_pair (Args&&... args)
 {
-	auto args_tuple = std::uses_allocator_construction_args<pair_t>(std::forward<Args>(args)...);
-	auto arg_count = std::tuple_size<decltype(args_tuple)>::value;
-	assert(arg_count == 3);
+    auto args_tuple = std::uses_allocator_construction_args<pair_t>(std::forward<Args>(args)...);
+    auto arg_count = std::tuple_size<decltype(args_tuple)>::value;
+    assert(arg_count == 3);
 }
 
 int main(int, char**)
 {
-	// non pair contruction
-	std::allocator<A> alloc;
-	auto a = std::make_obj_using_allocator<A>(alloc, 10, 5.0);
+    // non pair contruction
+    std::allocator<A> alloc;
+    auto a = std::make_obj_using_allocator<A>(alloc, 10, 5.0);
 
-	assert(a.a == 10);
-	assert(a.b == 5.0);
+    assert(a.a == 10);
+    assert(a.b == 5.0);
 
-	auto* b = std::uninitialized_construct_using_allocator(&a, alloc, 20, 10.0);
+    auto* b = std::uninitialized_construct_using_allocator(&a, alloc, 20, 10.0);
 
-	assert(a.a == 20);
-	assert(a.b == 10.0);
+    assert(a.a == 20);
+    assert(a.b == 10.0);
 
-	assert(b->a == 20);
-	assert(b->b == 10.0);
+    assert(b->a == 20);
+    assert(b->b == 10.0);
 
-	assert(&a == b);
+    assert(&a == b);
 
-	// pair construction
-	std::allocator<pair_t> p_alloc;
-	pair_t p { 10, 5.0 };
+    // pair construction
+    std::allocator<pair_t> p_alloc;
+    pair_t p { 10, 5.0 };
 
-	test_pair(p_alloc);
-	test_pair(p_alloc, 10, 5.0);
-	test_pair(p_alloc, p);
+    test_pair(p_alloc);
+    test_pair(p_alloc, 10, 5.0);
+    test_pair(p_alloc, p);
 
-	return 0;
+    return 0;
 }
