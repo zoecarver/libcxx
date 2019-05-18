7ddbbe27873358d2996b2dea625eb8e23b2c278a
zoe <z.zoelec2@gmail.com>
2019-05-18 16:01:34 -0700

    feat: fill in new shared_ptr

--- test/smart_ptr.pass.cpp
+++ test/smart_ptr.pass.cpp
@@ -1,30 +1,80 @@
 #include<__smart_ptr>
 #include<iostream>
 
-static int deleters_called = 0;
-static int count = 42;
-
-struct Foo
+struct B
 {
-    int val = count++;
-    ~Foo() {
-        val = -1;
-        deleters_called++;
-    }
+    static int count;
+
+    B() {++count;}
+    B(const B&) {++count;}
+    virtual ~B() {--count;}
 };
 
-void
-test()
-{
-    std::__smart::shared_ptr<Foo> sp(new Foo());
-    std::__smart::shared_ptr<Foo> sp1(sp);
-    assert((*sp1).val == 42);
-    assert((*sp).val == 42);
-}
+int B::count = 0;
 
-int main()
+struct A
+    : public B
 {
-    test();
+    static int count;
 
-    assert(deleters_called == 1);
+    A() {++count;}
+    A(const A&) {++count;}
+    ~A() {--count;}
+};
+
+int A::count = 0;
+
+struct C
+{
+    static int count;
+
+    C() {++count;}
+    C(const C&) {++count;}
+    virtual ~C() {--count;}
+};
+
+int C::count = 0;
+
+int main(int, char**)
+{
+    {
+        const std::__smart::shared_ptr<A> pA(new A);
+        assert(pA.use_count() == 1);
+        assert(B::count == 1);
+        assert(A::count == 1);
+        {
+            std::__smart::shared_ptr<B> pB(pA);
+            assert(B::count == 1);
+            assert(A::count == 1);
+            assert(pB.use_count() == 2);
+            assert(pA.use_count() == 2);
+            assert(pA.get() == pB.get());
+        }
+        assert(pA.use_count() == 1);
+        assert(B::count == 1);
+        assert(A::count == 1);
+    }
+    assert(B::count == 0);
+    assert(A::count == 0);
+    {
+        std::__smart::shared_ptr<A> pA;
+        assert(pA.use_count() == 0);
+        assert(B::count == 0);
+        assert(A::count == 0);
+        {
+            std::__smart::shared_ptr<B> pB(pA);
+            assert(B::count == 0);
+            assert(A::count == 0);
+            assert(pB.use_count() == 0);
+            assert(pA.use_count() == 0);
+            assert(pA.get() == pB.get());
+        }
+        assert(pA.use_count() == 0);
+        assert(B::count == 0);
+        assert(A::count == 0);
+    }
+    assert(B::count == 0);
+    assert(A::count == 0);
+
+  return 0;
 }
