a427bfe7aeaf058be7cbe2222c1f97a9149b2318
zoe <z.zoelec2@gmail.com>
2019-05-22 10:03:12 -0700

    chore: cleanup

--- test/std/utilities/memory/util.smartptr/util.smartptr.shared/util.smartptr.shared.const/pointer.pass.cpp
+++ test/std/utilities/memory/util.smartptr/util.smartptr.shared/util.smartptr.shared.const/pointer.pass.cpp
@@ -35,28 +35,14 @@ int main(int, char**)
     }
     assert(A::count == 0);
 
-//     {
-//         A* ptr = new A;
-//         std::shared_ptr<void> p(ptr);
-//         assert(A::count == 1);
-//         assert(p.use_count() == 1);
-//         assert(p.get() == ptr);
-//     }
-//     assert(A::count == 0);
-
-// TODO: add fail tests for when `delete[]` or `delete` does not work.
-// #if _LIBCPP_NEW_SHARED_PTR && false
-//     {
-//         A* ptr[8];
-//         for (unsigned i = 0; i < 8; ++i) ptr[i] = new A;
-//
-//         std::shared_ptr<A[8]> p(ptr);
-//         assert(A::count == 8);
-//         assert(p.use_count() == 1);
-//         assert(p.get() == ptr);
-//     }
-//     assert(A::count == 0);
-// #endif
+    {
+        A* ptr = new A;
+        std::shared_ptr<void> p(ptr);
+        assert(A::count == 1);
+        assert(p.use_count() == 1);
+        assert(p.get() == ptr);
+    }
+    assert(A::count == 0);
 
     return 0;
 }
