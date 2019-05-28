29f0dc5091028b092c5289292057d4054e6b08eb
zoe <z.zoelec2@gmail.com>
2019-05-28 16:57:54 -0700

    feat: add bounded array support to make_shared and allocate_shared

--- test/std/make-shared.pass.cpp
+++ test/std/make-shared.pass.cpp
@@ -29,65 +29,71 @@ int main()
         assert(A::count == 0);
     }
 
-//     {
-//         using T = A[8];
-//         std::shared_ptr<T> ptr0 = std::make_shared<T>();
-//         assert(A::count == 8);
-//
-//         std::shared_ptr<T> ptr1 = std::allocate_shared<T>(std::allocator<T>());
-//         assert(A::count == 8);
-//     }
-//     assert(A::count == 0);
+    {
+        using T = A[8];
 
-    // {
-//         using T = int[];
-//         std::shared_ptr<T> ptr0 = std::make_shared<T>(8, 42);
-//         std::shared_ptr<T> ptr1 = std::allocate_shared<T>(std::allocator<T>(), 8, 42);
-//
-//         for (unsigned i = 0; i < 8; ++i)
-//         {
-//             assert((*ptr0)[i] == 42);
-//             assert((*ptr1)[i] == 42);
-//         }
-//     }
-//
-//     {
-//         using T = int[8];
-//         std::shared_ptr<T> ptr0 = std::make_shared<T>(42);
-//         std::shared_ptr<T> ptr1 = std::allocate_shared<T>(std::allocator<T>(), 42);
-//
-//         for (unsigned i = 0; i < 8; ++i)
-//         {
-//             assert((*ptr0)[i] == 42);
-//             assert((*ptr1)[i] == 42);
-//         }
-//     }
-//
-//     {
-//         using T = int[][2];
-//         std::shared_ptr<T> ptr0 = std::make_shared<T>(8, {4, 2});
-//         std::shared_ptr<T> ptr1 = std::allocate_shared<T>(std::allocator<T>(), 8, {4, 2});
-//
-//         for (unsigned i = 0; i < 8; ++i)
-//         {
-//             assert((*ptr0)[i][0] == 4);
-//             assert((*ptr1)[i][0] == 4);
-//             assert((*ptr0)[i][1] == 2);
-//             assert((*ptr1)[i][1] == 2);
-//         }
-//     }
-//
-//     {
-//         using T = int[8][2];
-//         std::shared_ptr<T> ptr0 = std::make_shared<T>({4, 2});
-//         std::shared_ptr<T> ptr1 = std::allocate_shared<T>(std::allocator<T>(), {4, 2});
-//
-//         for (unsigned i = 0; i < 8; ++i)
-//         {
-//             assert((*ptr0)[i][0] == 4);
-//             assert((*ptr1)[i][0] == 4);
-//             assert((*ptr0)[i][1] == 2);
-//             assert((*ptr1)[i][1] == 2);
-//         }
-//     }
+        {
+            std::shared_ptr<T> ptr0 = std::make_shared<T>();
+            assert(A::count == 8);
+        }
+        assert(A::count == 0);
+
+        {
+            std::shared_ptr<T> ptr1 = std::allocate_shared<T>(std::allocator<T>());
+            assert(A::count == 8);
+        }
+        assert(A::count == 0);
+    }
+
+    {
+        using T = int[];
+        std::shared_ptr<T> ptr0 = std::make_shared<T>(8, 42);
+        std::shared_ptr<T> ptr1 = std::allocate_shared<T>(std::allocator<T>(), 8, 42);
+
+        for (unsigned i = 0; i < 8; ++i)
+        {
+            assert((*ptr0)[i] == 42);
+            assert((*ptr1)[i] == 42);
+        }
+    }
+
+    {
+        using T = int[8];
+        std::shared_ptr<T> ptr0 = std::make_shared<T>(42);
+        std::shared_ptr<T> ptr1 = std::allocate_shared<T>(std::allocator<T>(), 42);
+
+        for (unsigned i = 0; i < 8; ++i)
+        {
+            assert((*ptr0)[i] == 42);
+            assert((*ptr1)[i] == 42);
+        }
+    }
+
+    {
+        using T = int[][2];
+        std::shared_ptr<T> ptr0 = std::make_shared<T>(8, {4, 2});
+        std::shared_ptr<T> ptr1 = std::allocate_shared<T>(std::allocator<T>(), 8, {4, 2});
+
+        for (unsigned i = 0; i < 8; ++i)
+        {
+            assert((*ptr0)[i][0] == 4);
+            assert((*ptr1)[i][0] == 4);
+            assert((*ptr0)[i][1] == 2);
+            assert((*ptr1)[i][1] == 2);
+        }
+    }
+
+    {
+        using T = int[8][2];
+        std::shared_ptr<T> ptr0 = std::make_shared<T>({4, 2});
+        std::shared_ptr<T> ptr1 = std::allocate_shared<T>(std::allocator<T>(), {4, 2});
+
+        for (unsigned i = 0; i < 8; ++i)
+        {
+            assert((*ptr0)[i][0] == 4);
+            assert((*ptr1)[i][0] == 4);
+            assert((*ptr0)[i][1] == 2);
+            assert((*ptr1)[i][1] == 2);
+        }
+    }
 }
