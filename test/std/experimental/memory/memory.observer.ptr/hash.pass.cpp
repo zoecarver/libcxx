380c1374c77fa03fd1e2be7bc0db2fd58e8bfaa1
zoe <z.zoelec2@gmail.com>
2019-06-30 12:30:55 -0700

    fix: hash stuff

--- test/std/experimental/memory/memory.observer.ptr/hash.pass.cpp
+++ test/std/experimental/memory/memory.observer.ptr/hash.pass.cpp
@@ -22,17 +22,17 @@ struct A {};
 
 int main(int, char**)
 {
-//     {
-//         int* ptr = new int;
-//         std::experimental::observer_ptr<int> p(ptr);
-//         std::hash<std::experimental::observer_ptr<int>> f;
-//         std::size_t h = f(p);
-//         assert(h == std::hash<int*>()(ptr));
-//     }
-//     {
-//         test_hash_enabled_for_type<std::experimental::observer_ptr<int>>();
-//         test_hash_enabled_for_type<std::experimental::observer_ptr<A>>();
-//     }
+    {
+        int* ptr = new int;
+        std::experimental::observer_ptr<int> p(ptr);
+        std::hash<std::experimental::observer_ptr<int>> f;
+        std::size_t h = f(p);
+        assert(h == std::hash<int*>()(ptr));
+    }
+    {
+        test_hash_enabled_for_type<std::experimental::observer_ptr<int>>();
+        test_hash_enabled_for_type<std::experimental::observer_ptr<A>>();
+    }
 
     return 0;
 }
